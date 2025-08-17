a36a1d77efc757c6db3f37df4c6ea17bc87bf9a9
gaurav15042004 <gaurav15042004@gmail.com>
2025-08-17 15:11:49 +0530

    Daily practice

---
+++ dutchNationalFlagAlgo.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class dutchNationalFlagAlgo {
+    public static void Sorter(int[]arr, int n){
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+        while(mid <= high){
+            if(arr[mid] == 1){
+                mid++;
+            } else if (arr[mid] == 0) {
+                int temp = arr[mid];
+                arr[mid] = arr[low];
+                arr[low] =temp;
+                mid ++;
+                low++;
+            } else if (arr[mid] == 2) {
+                int temp1 = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp1;
+                high--;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+        int[] arr={2,1,1,2,0,0,0,2,1};
+        int n = arr.length;
+        Sorter(arr, n);
+    }
+}
