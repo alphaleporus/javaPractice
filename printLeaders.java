a52f5b229834af431b94848f50681604afca6b35
gaurav15042004 <gaurav15042004@gmail.com>
2025-08-23 16:25:35 +0530

    Daily practice

--- printLeaders.java
+++ printLeaders.java
@@ -1,20 +1,20 @@
 import java.util.ArrayList;
 
 public class printLeaders {
-    public static void findLeaders(int[]nums, ArrayList<Integer>Leaders){
+    public static void FindLeaders(int[]nums, ArrayList<Integer>leaders){
         int max = Integer.MIN_VALUE;
         for(int i = nums.length-1; i>=0; i--){
             max = Math.max(nums[i], max);
             if(nums[i]==max){
-                Leaders.add(nums[i]);
+                leaders.add(nums[i]);
             }
         }
     }
 
     public static void main(String[] args) {
         int[]nums = {10, 22, 12, 3, 0, 6};
-        ArrayList<Integer> Leaders = new ArrayList<>();
-        findLeaders(nums, Leaders);
-        System.out.println(Leaders);
+        ArrayList<Integer> leaders = new ArrayList<>();
+        FindLeaders(nums, leaders);
+        System.out.println(leaders);
     }
 }
