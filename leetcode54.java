b79fddc243a7263eb4b5c3e05e475fd32da414c4
gaurav15042004 <gaurav15042004@gmail.com>
2025-08-31 04:33:38 +0530

    Daily practice

---
+++ leetcode54.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class leetcode54 {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if(matrix==null || matrix.length==0){
+            return result;
+        }
+
+        int row = matrix.length;
+        int column = matrix[0].length;
+        int top = 0;
+        int bottom = row-1;
+        int left = 0;
+        int right = column-1;
+
+        while(top<=bottom && left<=right){
+            for(int i=left ; i<=right ; i++){
+                result.add(matrix[top][i]);
+            }
+            top++;
+
+            for(int i = top; i<=bottom; i++){
+                result.add(matrix[i][right]);
+            }
+            right--;
+
+            if(top<=bottom) {
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if(left<=right){
+                for(int i =bottom;i>=top;i--){
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+}
