8336c61619e5fc3ddb078f665811e667110e24a5
Gaurav Sharma <gaurav15042004@gmail.com>
2026-02-26 00:15:43 +0530

    Daily practice

--- leetcode347.java
+++ leetcode347.java
@@ -1,18 +1,37 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class leetcode347 {
-    public int[] topKFrequent(int[] nums, int k) {
-        List<Integer> result = new ArrayList<>();
+    public static int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> frequencyMap = new HashMap<>();
         for(int n:nums){
             frequencyMap.put(n, frequencyMap.getOrDefault(n,0)+1);
         }
 
-        Arrays.sort(frequencyMap.values().toArray());
-        for(int i = 0; i < k; i++){
+        PriorityQueue<Map.Entry<Integer,Integer>> heap = new PriorityQueue<>((a, b) ->
+                Integer.compare(a.getValue(), b.getValue()));
+        for(Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()){
+            int freq = entry.getValue();
+            int key = entry.getKey();
+            heap.add(entry);
+            if(heap.size() > k){
+                heap.poll();
+            }
         }
+        int[] result = new int[k];
+        for(int i =0; i<k;i++){
+            result[i] = heap.poll().getKey();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        leetcode347 obj = new leetcode347();
+        int[] nums = {1,1,1,2,2,3};
+        int k = 2;
+        int[] result = topKFrequent(nums ,k);
+        for(int i =0; i<k;i++){
+            System.out.print(result[i] + " ");
+        }
+
     }
 }
