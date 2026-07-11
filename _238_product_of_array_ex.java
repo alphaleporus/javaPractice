996ad803a2b003c14dff965f057a8f102e28e9e5
Gaurav Sharma <gaurav15042004@gmail.com>
2026-07-11 18:43:51 +0530

    solution for 238

---
+++ _238_product_of_array_ex.java
@@ -0,0 +1,16 @@
+public class _238_product_of_array_ex {
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        int product = 1;
+        for(int i=0; i<nums.length; i++){
+            result[i] = product;
+            product *= nums[i];
+        }
+        product = 1;
+        for(int i=nums.length-1; i>=0; i--){
+            result[i] *= product;
+            product *= nums[i];
+        }
+        return result;
+    }
+}
