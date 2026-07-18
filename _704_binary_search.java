872efefb02a43a3b279f02a490fd40b4f1f054db
Gaurav Sharma <gaurav15042004@gmail.com>
2026-07-18 21:50:45 +0530

    Solved 704 binary search

--- _704_binary_search.java
+++ _704_binary_search.java
@@ -1,13 +1,22 @@
 public class _704_binary_search {
     private int binarySearch(int[] nums, int target, int start, int end){
         int mid = (start + end)/2;
-        if(target == nums[mid]){
-            return mid;
-        }if-else(target < nums[mid]){
-
+        while(start!=end){
+            if(target == nums[mid]){
+                return mid;
+            }else if(target < nums[mid]){
+                return binarySearch(nums, target, start, mid);
+            }else{
+                return binarySearch(nums, target, mid+1, end);
+            }
         }
+        return -1;
     }
+
+
     public int search(int[] nums, int target) {
-        
+        int start = 0;
+        int end = nums.length-1;
+        return binarySearch(nums, target, start, end);
     }
 }
