f46eb9e97d901b8b5424027baa64e2999e0420da
Gaurav Sharma <gaurav15042004@gmail.com>
2026-07-20 00:17:28 +0530

    Resolved imcorrect naming bug

--- _74_search_2d_matrix.java
+++ _74_search_2d_matrix.java
@@ -1,23 +1,23 @@
 public class _74_search_2d_matrix {
-    private int binarySearch(int[][] nums, int target, int start, int end){
-        int cols = nums[0][0];
-        int mid = (start + end)/2;
-        while(start!=end){
-            if(target == nums[mid / cols][mid % cols]){
-                return mid;
-            }else if(target < nums[mid / cols][mid % cols]){
-                return binarySearch(nums, target, start, mid);
-            }else{
-                return binarySearch(nums, target, mid+1, end);
-            }
+    private boolean binarySearch(int[][] nums, int target, int start, int end){
+        if(start > end){
+            return false;
         }
-        return -1;
+        int cols = nums[0].length;
+        int mid = (start + end)/2;
+        if(target == nums[mid / cols][mid % cols]){
+            return true;
+        }else if(target < nums[mid / cols][mid % cols]){
+            return binarySearch(nums, target, start, mid-1);
+        }else{
+            return binarySearch(nums, target, mid+1, end);
+        }
     }
 
 
-    public int search(int[][] nums, int target) {
-        int start = nums[0][0];
-        int end = nums[nums.length-1][nums[0].length-1];
+    public boolean search(int[][] nums, int target) {
+        int start = 0;
+        int end = nums.length * nums[0].length-1;
         return binarySearch(nums, target, start, end);
     }
 }
